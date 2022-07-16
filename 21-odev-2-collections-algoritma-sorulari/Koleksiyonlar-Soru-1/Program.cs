d3ccfbd91d3c27089f61c412f7fc7a99d12c61e7
Ahmet ER <75838965+ahmet-er@users.noreply.github.com>
2022-07-16 20:30:55 +0300

    Add files via upload

---
+++ 21-odev-2-collections-algoritma-sorulari/Koleksiyonlar-Soru-1/Program.cs
@@ -0,0 +1,116 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Odev - 2 --- Soru - 1
+
+/*Soru - 1: Klavyeden girilen 20 adet pozitif sayının asal ve asal olmayan olarak 2 ayrı listeye atın. (ArrayList sınıfını kullanarak yazınız.)
+
+Negatif ve numeric olmayan girişleri engelleyin.
+Her bir dizinin elemanlarını büyükten küçüğe olacak şekilde ekrana yazdırın.
+Her iki dizinin eleman sayısını ve ortalamasını ekrana yazdırın.*/
+
+using System.Collections;
+
+ArrayList asalSayilar = new ArrayList();
+ArrayList asalOlmayanSayilar = new ArrayList();
+
+// giris yapilan sayilarin ArrayListe atanması ve hatalı giris kontrolu
+for (int i = 0; i < 20; i++)
+{
+    Console.Write("Lütfen {0}. sayiyi giriniz: ", i+1);
+    try
+    {
+        int sayi =Convert.ToInt32(Console.ReadLine());
+        if(sayi<=0)
+        {
+            Console.WriteLine("Lütfen sadece pozitif sayilar giriniz!");
+        }
+        else
+        {
+            if (AsalSayi(sayi))
+            {
+                asalSayilar.Add(sayi);
+            }
+            else
+            {
+                asalOlmayanSayilar.Add(sayi);
+            }
+        }
+            
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine("Lütfen sadece nümaratik degerler giriniz!");
+    }
+}
+
+Console.WriteLine("***** Asal Sayilar *****");
+foreach (var item in asalSayilar)
+    Console.WriteLine(item);
+
+Console.WriteLine("***** Asal Olmayan Sayilar *****");
+foreach (var item in asalOlmayanSayilar)
+    Console.WriteLine(item);
+
+// kücük -> büyük siralama
+asalSayilar.Sort();
+asalOlmayanSayilar.Sort();
+
+// Ters cevirme
+asalSayilar.Reverse();
+asalOlmayanSayilar.Reverse();
+
+Console.WriteLine("***** [Sıralı(büyük->kücük)] Asal Sayilar *****");
+foreach (var item in asalSayilar)
+    Console.WriteLine(item);
+
+Console.WriteLine("***** [Sıralı(büyük->kücük)] Asal Olmayan Sayilar *****");
+foreach (var item in asalOlmayanSayilar)
+    Console.WriteLine(item);
+
+// ArrayListlerin eleman sayilarini yazdirma
+Console.WriteLine("-----------------------------");
+Console.WriteLine("Asal Sayıların Sayisi: " + asalSayilar.Count);
+Console.WriteLine("Asal Olmayan Sayıların Sayisi: " + asalOlmayanSayilar.Count);
+
+// Eleman toplamlari
+int asalToplam = 0;
+int asalOlmayanToplam = 0;
+foreach (int item in asalSayilar)
+{
+    asalToplam += item;
+}
+foreach (int item in asalOlmayanSayilar)
+{
+    asalOlmayanToplam += item;
+}
+Console.WriteLine("-----------------------------");
+Console.WriteLine("Asal Sayıların Ortalamasi: " + (asalToplam / asalSayilar.Count));
+Console.WriteLine("Asal Olmayan Sayıların Ortalamasi: " + (asalOlmayanToplam / asalOlmayanSayilar.Count));
+
+
+// asal olup olmadigini bool olarak donduren methot
+static bool AsalSayi(int s)
+{
+    bool result = true;
+    for (int i = 2; i < s-1; i++)
+    {
+        if (s % i == 0)
+        {
+            result = false;
+            i=s;
+        }
+    }
+    if (s<2)
+    {
+        result=false;
+    }
+    return result;
+}
+
+
+
+
+
+
+
+Console.ReadKey();
---
+++ 21-odev-2-collections-algoritma-sorulari/Koleksiyonlar-Soru-2/Program.cs
@@ -0,0 +1,88 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Odev - 2 --- Soru - 2
+
+/*Soru - 2: Klavyeden girilen 20 adet sayının en büyük 3 tanesi ve en küçük 3 tanesi bulan, her iki grubun kendi içerisinde ortalamalarını alan ve bu ortalamaları ve ortalama toplamlarını console'a yazdıran programı yazınız. (Array sınıfını kullanarak yazınız.)*/
+
+using System.Collections;
+
+ArrayList sayilar = new ArrayList();
+int[] kucuk= new int[3];
+int[] buyuk= new int[3];
+
+for(int i=0;i<20;i++)
+{
+    
+    try
+    {
+        Console.Write("Lütfen {0}. sayiyi giriniz: ", i+1);
+        int sayi = int.Parse(Console.ReadLine());
+
+        if(sayi<=0)
+        {
+            Console.WriteLine("Lütfen sadece pozitif sayi giriniz!");
+        }
+        else
+        {
+            sayilar.Add(sayi);
+        }
+    }
+    catch (System.Exception)
+    {
+        Console.WriteLine("Lütfen sadece nümaretik değer giriniz!");
+        //throw;
+    }
+}
+Console.WriteLine("***** İlk yazdirma *****");
+// Yazdirma
+foreach (var item in sayilar)
+{
+    Console.WriteLine(item);
+}
+
+sayilar.Sort();
+
+for (int i = 0; i < 3; i++)
+{
+    kucuk[i] = (int)sayilar[i];
+}
+
+
+Console.WriteLine("***** En kücük üclü *****");
+// en kücük ilk 3 lü
+foreach (var item in kucuk)
+{
+    Console.WriteLine(item);
+}
+
+sayilar.Reverse();
+for (int i = 0; i < 3; i++)
+{
+    buyuk[i] = (int)sayilar[i];
+}
+
+Console.WriteLine("***** En büyük üclü *****");
+// en kücük ilk 3 lü
+foreach (var item in buyuk)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("---------");
+int kTop=0;
+int bTop=0;
+for (int i = 0; i < kucuk.Length; i++)
+{
+    kTop += kucuk[i];
+    bTop += buyuk[i];
+}
+
+Console.WriteLine("***** Üclülerin Ortalamalari *****");
+Console.WriteLine("En Kücük Üclünün ortalamasi = " + (kTop/3));
+Console.WriteLine("---------");
+Console.WriteLine("En Büyük Üclünün ortalamasi = " + (bTop/3));
+
+
+
+
+Console.ReadKey();
